f8b694779b5e1913d6713a252f1136c2fcdb4777
trash-and-fire <69399722+trash-and-fire@users.noreply.github.com>
2021-02-23 02:24:05 +0300

    apply codestyle

--- webpack.config.js
+++ webpack.config.js
@@ -6,67 +6,65 @@ const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
 
 module.exports = {
-	entry: {
-		'build/bundle': ['./src/main.ts']
-	},
-	resolve: {
-		alias: {
-			svelte: path.dirname(require.resolve('svelte/package.json')),
-			'lightweight-charts': 'lightweight-charts/dist/lightweight-charts.esm.development.js'
-		},
-		extensions: ['.mjs', '.js', '.ts', '.svelte'],
-		mainFields: ['svelte', 'browser', 'module', 'main']
-	},
-	output: {
-		path: path.join(__dirname, '/public'),
-		filename: '[name].js',
-		chunkFilename: '[name].[id].js'
-	},
-	module: {
-			rules: [
-				{
-					test: /\.ts$/,
-					loader: 'ts-loader',
-					exclude: /node_modules/
-				},
-				{
-				test: /\.svelte$/,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						compilerOptions: {
-							dev: !prod
-						},
-						emitCss: prod,
-						hotReload: !prod,
-							preprocess: sveltePreprocess({ sourceMap: !prod })
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader'
-				]
-			},
-			{
-				// required to prevent errors from Svelte on Webpack 5+
-				test: /node_modules\/svelte\/.*\.mjs$/,
-				resolve: {
-					fullySpecified: false
-				}
-			}
-		]
-	},
-	mode,
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		})
-	],
-	devtool: prod ? false : 'source-map',
-	devServer: {
-		hot: true
-	}
+    entry: {
+        'build/bundle': ['./src/main.ts']
+    },
+    resolve: {
+        alias: {
+            'lightweight-charts': 'lightweight-charts/dist/lightweight-charts.esm.development.js'
+        },
+        extensions: ['.mjs', '.js', '.ts', '.svelte'],
+    },
+    output: {
+        path: path.join(__dirname, '/public'),
+        filename: '[name].js',
+        chunkFilename: '[name].[id].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.svelte$/,
+                use: {
+                    loader: 'svelte-loader',
+                    options: {
+                        compilerOptions: {
+                            dev: !prod
+                        },
+                        emitCss: prod,
+                        hotReload: !prod,
+                        preprocess: sveltePreprocess({sourceMap: !prod})
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                // required to prevent errors from Svelte on Webpack 5+
+                test: /node_modules\/svelte\/.*\.mjs$/,
+                resolve: {
+                    fullySpecified: false
+                }
+            }
+        ]
+    },
+    mode,
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    devtool: prod ? false : 'source-map',
+    devServer: {
+        hot: true
+    }
 };
