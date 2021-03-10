c92af0e19414f9b22d9ff20c708b53160932160b
trash-and-fire <69399722+trash-and-fire@users.noreply.github.com>
2021-03-11 02:11:23 +0300

    remove garbage from svelte files

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,10 @@
 const {src, dest, parallel} = require('gulp');
 // const typescript = require('gulp-typescript');
-const {Transform} = require('stream');
 const rename = require('gulp-rename');
 const transform = require('gulp-transform');
-const {compile, preprocess} = require('svelte/compiler');
-const PluginError = require('plugin-error');
+const {preprocess} = require('svelte/compiler');
 
 // const tsProject = typescript.createProject('tsconfig.build.json');
-
-function svelteTransform() {
-    return new Transform({
-        objectMode: true,
-        async transform(file, enc, cb) {
-            if (file.isNull()) {
-                cb(null, file);
-                return;
-            }
-
-            try {
-                const source = file.contents.toString();
-
-                const options = require('svelte-preprocess')({sourceMap: false});
-                options.filename = file.path;
-                const preprocessed = await preprocess(source, options);
-                const {js} = compile(preprocessed.toString(), { filename: file.path });
-                file.contents = Buffer.from(js.code);
-                file.extname = '.svelte.js';
-
-                cb(null, file);
-            } catch (err) {
-                if (file.path) {
-                    err.fileName = file.path;
-                }
-
-                cb(new PluginError('gulp-svelte', err));
-            }
-        }
-    });
-}
 
 // function scripts() {
 //     return src(['./src/package/**/*.ts'])
@@ -63,9 +30,27 @@ function typings() {
         .pipe(dest('./src/package/dist'));
 }
 
+async function applyPreprocess(content, file) {
+    const ts = require('svelte-preprocess').typescript();
+    ts.filename = file.basename;
+    // TODO: remove source mapping url
+    const garbage = require('svelte-preprocess').replace([
+        [/<!--[^]*?-->|<script(\s[^]*?)?(?:>([^]*?)<\/script>|\/>)/gi, (value, attributes) => {
+            if (attributes !== undefined && attributes.includes('lang="ts"')) {
+                return value.replace('lang="ts"', '');
+            }
+            return value;
+        }]
+    ]);
+    garbage.filename = file.basename;
+    content = await preprocess(content, ts);
+    content = await preprocess(content.code, garbage);
+    return content.code;
+}
+
 function svelte() {
     return src(['./src/package/**/*.svelte'])
-        .pipe(svelteTransform())
+        .pipe(transform('utf8', applyPreprocess))
         .pipe(dest('./src/package/dist'));
 }
 
