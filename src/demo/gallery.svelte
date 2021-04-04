0e9209f5a50331d9529959accd0643a8b81523cf
trash-and-fire <69399722+trash-and-fire@users.noreply.github.com>
2021-04-04 23:22:45 +0300

    link draft

--- src/demo/gallery.svelte
+++ src/demo/gallery.svelte
@@ -1,29 +1,30 @@
 <script>
-    import BarChart from './samples/bar-chart.svelte';
-    import CandlestickChart from './samples/candlestick-chart.svelte';
-    import CustomFontFamily from './samples/custom-font-family.svelte';
-    import CustomPriceFormatter from './samples/custom-price-formatter.svelte';
-    import CustomLocale from './samples/custom-locale.svelte';
-    import CustomThemes from './samples/custom-themes.svelte';
-    import CustomWatermark from './samples/custom-watermark.svelte';
-    import Legend from './samples/legend.svelte';
+    import repl from './repl.json';
+    // eslint-disable-next-line no-undef
+    const context = require.context('./samples', false, /\.svelte$/);
 
-    const components = [
-        BarChart,
-        CandlestickChart,
-        CustomFontFamily,
-        CustomPriceFormatter,
-        CustomLocale,
-        CustomThemes,
-        CustomWatermark,
-        Legend,
-    ];
+    // eslint-disable-next-line @typescript-eslint/typedef
+    const components = context.keys().map((request) => {
+        const [file] = /(\w|[-.])+$/.exec(request);
+        const id = repl.samples[file].uid;
+        return {
+            href: `https://svelte.dev/repl/${id}`,
+            constructor: context(request).default,
+        }
+    });
 </script>
 
 <div class="container">
     {#each components as component (component)}
         <div class="item">
-            <svelte:component this={component}/>
+            <a class="link" target="_blank" href={component.href} title="Show in REPL">
+                <svg width="18" height="15" viewBox="0 0 18 15" xmlns="http://www.w3.org/2000/svg">
+                    <g stroke="currentColor" fill="none">
+                        <path d="M12.5 2.5l5 5-5 5M10.5.5l-3 14M5.5 2.5l-5 5 5 5"></path>
+                    </g>
+                </svg>
+            </a>
+            <svelte:component this={component.constructor}/>
         </div>
     {/each}
 </div>
@@ -40,7 +41,29 @@
     }
 
     .item {
+        position: relative;
         flex: none;
         margin: 0 20px 0;
+        z-index: 0;
+    }
+
+    .link:link,
+    .link:visited {
+        position: absolute;
+        color: inherit;
+        display: flex;
+        font-size: 2em;
+        top: 0.67em;
+        right: 0;
+        height: 34px;
+        width: 34px;
+        border-radius: 50%;
+        padding: 7px;
+        align-items: center;
+        justify-content: center;
+        box-sizing: border-box;
+    }
+    .link:hover {
+        background: #f2f3f5;
     }
 </style>
