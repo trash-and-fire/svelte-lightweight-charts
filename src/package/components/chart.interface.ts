cc409b17cf856337fda29ffe8e1bfd682fdf8635
trash-and-fire <69399722+trash-and-fire@users.noreply.github.com>
2022-04-15 18:58:07 +0300

    provide ref callback and rest spread for dom container

--- src/package/components/chart.interface.ts
+++ src/package/components/chart.interface.ts
@@ -8,6 +8,11 @@ import type {Reference} from '../types';
 
 export interface $$PROPS extends DeepPartial<ChartOptions> {
     ref?: Reference<IChartApi>;
+    container?: {
+        ref?: Reference<HTMLElement>;
+        class?: string;
+        id?: string;
+    }
 }
 
 export interface $$EVENTS {
