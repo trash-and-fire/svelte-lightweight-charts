46f82d014d5e99cd27a5712c04dad92750b863df
trash-and-fire <69399722+trash-and-fire@users.noreply.github.com>
2022-04-18 17:39:10 +0300

    immutable time-scale

--- src/package/components/time-scale.svelte
+++ src/package/components/time-scale.svelte
@@ -1,3 +1,5 @@
+<svelte:options immutable={true}/>
+
 <script lang="ts">
     import type {LogicalRange, TimeRange} from 'lightweight-charts';
     import type {$$PROPS, $$EVENTS} from './time-scale.interface';
